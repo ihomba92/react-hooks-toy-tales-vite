f28d1c45c26e48f6e1749d172381d4447855d2fb
ihomba92 <ihombakibandidavid@gmail.com>
2026-05-16 19:56:32 +0300

    feat: added props, featch and state

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys}) {
+
+function showToys() {
+  return toys.map((toy) => (
+    <ToyCard key={toy.id} toy={toy} />
+  ))
+}
+  
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection"
+    >{
+      showToys()
+    }</div>
   );
 }
 
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 
 function ToyForm() {
+
+const [newToy, setNewToy] = useState({})
+  
+  function handleSubmit(event) {
+  event.preventDefault();
+  }
+  const newToy ={
+    name: "name",
+    image: "image",
+    likes: 0
+  }
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form"
+        onSubmit={handleSubmit}
+        data-testid="add-toy-form">
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={newToy.name}
+          onChange={(event) => setNewToy({...newToy, name: event.target.value})}
         />
         <br />
         <input
@@ -17,6 +32,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={newToy.image}
+          onChange={(event) => setNewToy({...newToy, image: event.target.value})}
         />
         <br />
         <input
