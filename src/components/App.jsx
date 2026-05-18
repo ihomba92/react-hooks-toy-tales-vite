562773d8e1b607b20e976814c36a938fc7438f0c
ihomba92 <ihombakibandidavid@gmail.com>
2026-05-18 10:23:30 +0300

    Feat: added form, and CRUD operations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
-// import ToyForm from "./ToyForm";
+import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
-const[toys, setToys] = useState([]);
+  const [toys, setToys] = useState([])
+
+  useEffect(()=>{
+    async function fetchToys(){
+      const response = await fetch ("http://localhost:3001/toys")
+      const toysData = await response.json()
+      setToys(toysData)
+    }
+    fetchToys()
+
+  }, []);
+
+  function handleAddToy(newToy){
+     setToys([...toys, newToy])
+  }
+
+  function handleDeleteToy(toyId){
+    setToys(toys.filter((toy)=>toy.id !==toyId))
+  }
+
+  function handleLikeToy(updatedToy){
+    setToys(toys.map((toy)=>(toy.id ===updatedToy.id? updatedToy : toy)))
+  }
+
   function handleClick() {
+   
     setShowForm((showForm) => !showForm);
   }
-  useEffect(() => {
-    fetch("http://localhost:3001/toys")
-      .then((response) => response.json())
-      .then((data) => setToys(data));
-  }, []);
 
   return (
     <>
       <Header />
-      {/* {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy ={handleAddToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
-      </div> */}
-      <ToyContainer toys={toys} />
+      </div>
+      <ToyContainer 
+      toys ={toys}
+      onDeleteToy ={handleDeleteToy}
+      onLikeToy={handleLikeToy}
+       />
     </>
   );
 }
--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 
-function ToyCard( {toy}) {
+function ToyCard({toy, onDeleteToy, onLikeToy}) {
+ 
+  async function handleLike() {
+    const response = await fetch (`http://localhost:3001/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: { 'Content-Type': "application/json"},
+      body:JSON.stringify({likes:toy.likes +1})
+    })
+    const updatedToy = await response.json()
+    onLikeToy(updatedToy)
+  }
+
+  async function handleDelete(){
+    await fetch (`http://localhost:3001/toys/${toy.id}`, {
+      method: "DELETE",
+    })
+    onDeleteToy(toy.id)
+  }
+
   return (
     <div className="card" data-testid="toy-card">
       <h2>{toy.name}</h2>
       <img
         src={toy.image}
         alt={toy.name}
-        alt={toy.name}
         className="toy-avatar"
       />
       <p>{toy.likes} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <button className="like-btn" onClick={handleLike}>Like {"<3"}</button>
+      <button className="del-btn" onClick= {handleDelete}>Donate to GoodWill</button>
     </div>
   );
 }
